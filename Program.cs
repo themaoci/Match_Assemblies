70113d814281849176b5730ce2a2806b2d638606
TheSxW <themaoci@gmail.com>
2021-08-02 11:37:44 +0200

    initial code

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match_Assemblies
+{
+    class Program
+    {
+        static bool DEBUG = true;
+        static bool searchingForGClasses(IList<TypeDef> types, ref int searching, gclassSearch searchClass, ref string Output) {
+            var FilteredTypes = types.Where(type => type.Name.StartsWith(searchClass.StartingWith));
+            foreach (TypeDef type in FilteredTypes)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (searchClass.methodsItContain.Contains(method.Name))
+                    {
+                        if(DEBUG)
+                            Console.WriteLine($"method.Name:{method.Name}, {type.FullName}");
+                        searching++;
+                        
+                        if (searching == searchClass.CountMatches)
+                        {
+                            Output = $"{type.Name}";
+                            
+                            return searching == searchClass.CountMatches;
+                        }
+                    }
+                }
+                foreach (var property in type.Properties)
+                {
+                    if (searchClass.propertiesItContain.Contains(property.Name))
+                    {
+                        Console.WriteLine(property.Name);
+                        searching++;
+                        if (searching == searchClass.CountMatches)
+                        {
+                            Output = $"{type.Name}";
+
+                            return searching == searchClass.CountMatches;
+                        }
+                    }
+                }
+            }
+            return searching == searchClass.CountMatches;
+        }
+        class gclassSearch {
+            public gclassSearch(string originalName, string startingWith, List<string> MethodsToLookFor, List<string> PropertiesToLookFor) 
+            {
+                OriginalName = originalName;
+                StartingWith = startingWith;
+                methodsItContain = MethodsToLookFor;
+                propertiesItContain = PropertiesToLookFor;
+            }
+            public int CountMatches {
+                get 
+                {
+                    return methodsItContain.Count + propertiesItContain.Count;
+                }
+            }
+            public string OriginalName = "";
+            public string StartingWith = "";
+            public List<string> methodsItContain = new List<string>();
+            public List<string> propertiesItContain = new List<string>();
+        }
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assembly-CSharp_o.dll");
+            var assembly = ModuleDefMD.Load(path);
+
+            List<gclassSearch> ListOfSearchings = new List<gclassSearch>();
+            ListOfSearchings.Add(new gclassSearch(
+                "MainMenuController", "GClass",
+                new List<string> { "OnProfileChangeApplied", "ShowScreen" },
+                new List<string> { "SelectedLocation", "InventoryController" }));
+            ListOfSearchings.Add(new gclassSearch(
+                "IHealthController", "GInterface",
+                new List<string> { "GetBodyPartsInCriticalCondition", "AddImmunityNotificationEffect", "PropagateAllEffects" },
+                new List<string> { "DamageCoeff", "TemperatureRate", "CarryingWeightRelativeModifier" }));
+            //var types = Original_Assembly.GetType(findName);
+            //var getTypes = .Single(Class => Class.Name == findName);
+            var types = assembly.Types; //.Single(type => type.Name == findName);
+            if(DEBUG)
+                Console.WriteLine(types.Count());
+            Console.WriteLine("Current => New");
+            foreach (var gclassSearch in ListOfSearchings) {
+                int searching = 0;
+                string Output = "";
+
+                if (searchingForGClasses(types, ref searching, gclassSearch, ref Output))
+                {
+                    Console.WriteLine(gclassSearch.OriginalName + " => " + Output);
+                    Console.WriteLine(" ");
+                }
+
+            }
+
+            Console.WriteLine("Finished... Waiting on key press...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Match_Assemblies")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Match_Assemblies")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97b3482f-45a7-41f4-8673-1e29ed1b50ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
